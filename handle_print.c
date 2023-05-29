23121bd76660f2938eae9e9cab127803a75c0f72
khaoula <khaoula@gmail.com>
2023-05-29 13:27:19 -0700

    patner prog

--- handle_print.c
+++ handle_print.c
@@ -1,49 +1,104 @@
 #include "main.h"
-/**
- * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
- * @ind: ind.
- * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: 1 or 2;
- */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	int i, unknow_len = 0, printed_chars = -1;
-	fmt_t fmt_types[] = {
-		{'c', print_char}, {'s', print_string}, {'%', print_percent},
-		{'i', print_int}, {'d', print_int}, {'b', print_binary},
-		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
-		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
-		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
-	};
-	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+#include <stdarg.h>
+#include <stdio.h> // Example header file inclusion
 
-	if (fmt_types[i].fmt == '\0')
-	{
-		if (fmt[*ind] == '\0')
-			return (-1);
-		unknow_len += write(1, "%%", 1);
-		if (fmt[*ind - 1] == ' ')
-			unknow_len += write(1, " ", 1);
-		else if (width)
-		{
-			--(*ind);
-			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-				--(*ind);
-			if (fmt[*ind] == ' ')
-				--(*ind);
-			return (1);
-		}
-		unknow_len += write(1, &fmt[*ind], 1);
-		return (unknow_len);
-	}
-	return (printed_chars);
+// Function prototypes
+int print_char(va_list list, char buffer[], int flags, int width, int precision, int size);
+int print_string(va_list list, char buffer[], int flags, int width, int precision, int size);
+int print_percent(va_list list, char buffer[], int flags, int width, int precision, int size);
+int print_int(va_list list, char buffer[], int flags, int width, int precision, int size);
+int print_binary(va_list list, char buffer[], int flags, int width, int precision, int size);
+
+// Definition of fmt_t structure
+typedef struct {
+    char fmt;
+    int (*fn)(va_list, char[], int, int, int, int);
+} fmt_t;
+
+// Print functions implementation
+int print_char(va_list list, char buffer[], int flags, int width, int precision, int size) {
+    // Example implementation
+    char c = va_arg(list, int);
+    // Handle printing the character as per the specified flags, width, precision, and size
+    // ...
+    return 0; // Return the number of characters printed
+}
+
+int print_string(va_list list, char buffer[], int flags, int width, int precision, int size) {
+    // Example implementation
+    char *str = va_arg(list, char*);
+    // Handle printing the string as per the specified flags, width, precision, and size
+    // ...
+    return 0; // Return the number of characters printed
+}
+
+int print_percent(va_list list, char buffer[], int flags, int width, int precision, int size) {
+    // Example implementation
+    // Handle printing the percent symbol as per the specified flags, width, precision, and size
+    // ...
+    return 0; // Return the number of characters printed
+}
+
+int print_int(va_list list, char buffer[], int flags, int width, int precision, int size) {
+    // Example implementation
+    int num = va_arg(list, int);
+    // Handle printing the integer as per the specified flags, width, precision, and size
+    // ...
+    return 0; // Return the number of characters printed
+}
+
+int print_binary(va_list list, char buffer[], int flags, int width, int precision, int size) {
+    // Example implementation
+    int num = va_arg(list, int);
+    // Handle printing the binary representation of the number as per the specified flags, width, precision, and size
+    // ...
+    return 0; // Return the number of characters printed
+}
+
+// handle_print function
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+    int flags, int width, int precision, int size) {
+    int i, unknow_len = 0, printed_chars = -1;
+    fmt_t fmt_types[] = {
+        {'c', print_char}, {'s', print_string}, {'%', print_percent},
+        {'i', print_int}, {'d', print_int}, {'b', print_binary},
+        {'u', NULL}, {'o', NULL}, {'x', NULL},
+        {'X', NULL}, {'p', NULL}, {'S', NULL},
+        {'r', NULL}, {'R', NULL}, {'\0', NULL}
+    };
+
+    for (i = 0; fmt_types[i].fmt != '\0'; i++) {
+        if (fmt[*ind] == fmt_types[i].fmt) {
+            if (fmt_types[i].fn != NULL) {
+                return fmt_types[i].fn(list, buffer, flags, width, precision, size);
+            } else {
+                // Handle unsupported format specifier
+                // ...
+                return -1;
+            }
+        }
+    }
+
+    if (fmt_types[i].fmt == '\0') {
+        if (fmt[*ind] == '\0') {
+            return -1;
+        }
+        unknow_len += write(1, "%%", 1);
+        if (fmt[*ind - 1] == ' ') {
+            unknow_len += write(1, " ", 1);
+        } else if (width) {
+            --(*ind);
+            while (fmt[*ind] != ' ' && fmt[*ind] != '%') {
+                --(*ind);
+            }
+            if (fmt[*ind] == ' ') {
+                --(*ind);
+            }
+            return 1;
+        }
+        unknow_len += write(1, &fmt[*ind], 1);
+        return unknow_len;
+    }
+
+    return printed_chars;
 }
